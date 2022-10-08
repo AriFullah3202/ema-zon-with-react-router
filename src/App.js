b2b23cadf284594fe18ededd78eaab2026593b5a
Arif <marifullaah@gmail.com>
2022-10-08 11:23:14 +0600

    added link every page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import { RouterProvider } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
+import Main from './Layout/Main';
+import About from './components/About/About';
+import Orders from './components/Orders/Orders';
+import inventory from './components/inventory/Inventory';
+import Inventory from './components/inventory/Inventory';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Shop></Shop>
+        },
+        {
+          path: "/shop",
+          element: <Shop></Shop>
+        },
+        {
+          path: "about",
+          element: <About></About>
+        },
+        {
+          path: "orders",
+          element: <Orders></Orders>
+        },
+        {
+          path: "inventory",
+          element: <Inventory></Inventory>
+        }
+      ]
+    }
+
+  ])
   return (
     <div>
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
